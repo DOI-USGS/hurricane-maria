94f2c1d1dd237c2e606c37e854885dfafad78495
Alison Appling <aappling@usgs.gov>
2017-09-19 15:12:01 -0700

    annotate process.storm_location, hurricaneTrack; use Puerto Rico tz in fetch.timestamps

--- scripts/fetch/fetch.timesteps.R
+++ scripts/fetch/fetch.timesteps.R
@@ -1,13 +1,13 @@
-
+#' Create a JSON file of timesteps to use in the animation
 fetch.timesteps <- function(viz = as.viz('timesteps')){
   library(dplyr)
   times <- 
     # readDepends(viz)[["precip-cell-data"]] %>% # DO THIS WHEN PRECIP IS AVAILBLE
-    data.frame(DateTime=seq(
-      as.POSIXct('2017-09-18 00:00:00'),
-      as.POSIXct('2017-09-20 00:00:00'),
-      by=as.difftime(1, units='hours'))) %>% # PLACEHOLDER UNTIL PRECIP DATA
     # select(DateTime) %>% 
+    data.frame(DateTime=seq(
+      as.POSIXct('2017-09-18 00:00:00', tz = "America/Puerto_Rico"),
+      as.POSIXct('2017-09-20 00:00:00', tz = "America/Puerto_Rico"),
+      by=as.difftime(1, units='hours'))) %>% # PLACEHOLDER UNTIL PRECIP DATA
     unique() %>% 
     pull(DateTime) %>% 
     as.POSIXct %>% 
