6cd9b609f4e3169a72b55fedc5b1f840ea0d002f
David Blodgett <dblodgett@usgs.gov>
2017-09-19 14:59:38 -0500

    narrower file and commented code to remove

--- scripts/fetch/fetch.precip_shapefiles.R
+++ scripts/fetch/fetch.precip_shapefiles.R
@@ -2,19 +2,16 @@ fetchTimestamp.precip_shapefiles <- vizlab:::fetchTimestamp.file
 
 fetch.precip_shapefiles <- function(viz = as.viz('precip_shapefiles')){
   
-  # if(!viz[["refetch"]]) {
-  #   return(NULL)
-  # }
-  
   seDates <- readDepends(viz)[["dates"]]
   startDate <- as.Date(seDates$startDate)
   endDate <- as.Date(seDates$endDate)
   dates <- seq(startDate, endDate, by="days")
   
   precip_page_url_base <- viz[["url_base"]]
-  precip_page_urls <- paste0(precip_page_url_base, as.character(dates, format="%Y%m%d"), "/")
+  precip_page_urls <- paste0(precip_page_url_base, 
+                             as.character(dates, format="%Y%m%d"), "/")
   
-  tempdir <- viz[["location"]]
+  precip_dir <- viz[["location"]]
   
   for(precip_page_url in precip_page_urls) {
     
@@ -27,7 +24,7 @@ fetch.precip_shapefiles <- function(viz = as.viz('precip_shapefiles')){
     precip_files <- precip_files[which(grepl("tar.gz", precip_files))]
 
     for(dFile in precip_files) {
-      outfile <- file.path(tempdir, dFile)
+      outfile <- file.path(precip_dir, dFile)
       if(!file.exists(outfile)) download.file(paste0(precip_page_url, dFile), 
                                               outfile)
     }
