bd7ba7c307daa9e571a852486b34e017b051a0d4
Alison Appling <aappling@usgs.gov>
2017-09-20 11:33:15 -0700

    keep an internal eye on gages & data flags

---
+++ scripts/visualize/visualize.monitor_gages.R
@@ -0,0 +1,10 @@
+visualize.monitor_gages <- function(viz=as.viz('monitor_gages')) {
+  library(ggplot2)
+  gage <- readDepends(viz)[['gage-data']]
+  g <- ggplot(gage, aes(x=dateTime, y=p_Inst, color=p_Inst_cd)) +
+    geom_line() + 
+    scale_x_datetime(date_breaks = '24 hours', date_labels="%m/%d") +
+    facet_wrap(~ site_no, scales='free_y') + 
+    theme_classic()
+  ggsave(filename=viz$location, plot=g)
+}
