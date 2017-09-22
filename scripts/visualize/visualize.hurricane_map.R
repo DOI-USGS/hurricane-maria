61e13bec904089b58a9131c867675667d434df41
Jordan S Read <jread@usgs.gov>
2017-09-22 09:04:33 -0500

    requested text changes

--- scripts/visualize/visualize.hurricane_map.R
+++ scripts/visualize/visualize.hurricane_map.R
@@ -174,7 +174,7 @@ visualize.hurricane_map_landscape <- function(viz = as.viz('hurricane-map-landsc
 add_legend <- function(parent.ele, colors, break.step){
   
   # lower left legend:
-  xml_add_child(parent.ele, 'text', "Total rainfall amount (inches)", class='svg-text legend-text', dy="-1em",
+  xml_add_child(parent.ele, 'text', "Total rainfall amount (inches; NOAA)", class='svg-text legend-text', dy="-1em",
                 transform="translate(0,35)")
   g.rains <- xml_add_child(parent.ele, 'g', id = 'rain-legend')
   g.hurr <- xml_add_child(parent.ele, 'g', id = 'hurr-legend', transform="translate(15,-80)")
@@ -194,7 +194,7 @@ add_legend <- function(parent.ele, colors, break.step){
                 transform = "scale(0.6)")
   xml_add_child(g.gage_out, 'text', "No data", class='svg-text legend-text', dx='20', dy="0.33em")
   
-  g.main_gage_text <- xml_add_child(parent.ele, 'text', "USGS Stream Gages (< 1% of US total)", class='svg-text legend-text', dy="-1em",
+  g.main_gage_text <- xml_add_child(parent.ele, 'text', "USGS Stream Gages (< 25% of Puerto Rico gages)", class='svg-text legend-text', dy="-1em",
                                     transform="translate(0,-38)")
   
   
